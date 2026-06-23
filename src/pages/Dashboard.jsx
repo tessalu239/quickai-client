96582375daf429ef6c0c5286e39c3559d0ce198f
Tessa <new@0e:bb:16:90:08:51.home>
2026-06-23 15:59:06 -0400

    Add communities + dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,43 +1,65 @@
 /* eslint-disable react-hooks/set-state-in-effect */
-import React, { useEffect, useState } from 'react'
-import { dummyCreationData } from '../assets/assets'
-import { Sparkle } from 'lucide-react'
-import CreationItem from '../components/CreationItem'
+import React, { useEffect, useState } from "react";
+import { Sparkle } from "lucide-react";
+import CreationItem from "../components/CreationItem";
+import axios from "axios";
+import { useAuth } from "@clerk/clerk-react";
+import toast from "react-hot-toast";
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 const Dashboard = () => {
-  const [creation, setCreation]= useState([])
+  const [creation, setCreation] = useState([]);
 
-  const getDashboardData = async ()=>{
-    setCreation(dummyCreationData)
-  }
-  useEffect(()=>{ 
-    getDashboardData()
-  },[]
-  )
+  const [loading, setLoading] = useState(true);
+  const { getToken } = useAuth();
+
+  const getDashboardData = async () => {
+    try {
+      const { data } = await axios.get("/api/user/get-user-creations", {
+        headers: { Authorization: `Bearer ${await getToken()}` },
+      });
+
+      if (data.success) {
+        setCreation(data.creations);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (e) {
+      toast.error(e.message);
+    }
+    setLoading(false);
+  };
+  useEffect(() => {
+    getDashboardData();
+  }, []);
   return (
-    <div className='h-full overflow-y-scroll p-6'>
-        <div className='flex justify-start gap-4 flex-wrap'>
-          <div className="flex justify-between items-center w-72 p-4 px-6 bg-white rounded-xl border border-gray-200">
-            <div className='text-slate-600'>
-
-            <p className='text-sm'>Total Creation</p>
-            <h2 className='text-xl font-semibold'>{creation.length}</h2>
-            </div>
-            <div className='w-10 h-10 rounded-lg bg-gradient-to-br from-[#3588F2] to-[#0BB0D7] text-white flex justify-center items-center'>
-              <Sparkle className='text-white w-5'/>
-            </div>
+    <div className="h-full overflow-y-scroll p-6">
+      <div className="flex justify-start gap-4 flex-wrap">
+        <div className="flex justify-between items-center w-72 p-4 px-6 bg-white rounded-xl border border-gray-200">
+          <div className="text-slate-600">
+            <p className="text-sm">Total Creation</p>
+            <h2 className="text-xl font-semibold">{creation.length}</h2>
+          </div>
+          <div className="w-10 h-10 rounded-lg bg-gradient-to-br from-[#3588F2] to-[#0BB0D7] text-white flex justify-center items-center">
+            <Sparkle className="text-white w-5" />
           </div>
         </div>
-        <div className='space-y-3'>
-          <p className='mt-6 mb-4'>Recent Creations</p>
-          {
-            creation.map((item)=>(
-              <CreationItem key={item.id} item={item}/>
-            ))
-          }
+      </div>
+      {loading ? (
+        <div className="flex justify-center items-center h-full">
+          <span className="w-10 h-10 my-1 rounded-full border-3 border-primary border-t-transparent animate-spin"></span>
         </div>
+      ) : (
+        <div className="space-y-3">
+          <p className="mt-6 mb-4">Recent Creations</p>
+          {creation.map((item) => (
+            <CreationItem key={item.id} item={item} />
+          ))}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
